dd66df71ade5a13845eed19ecec21dbe0ac49805
Daniel Moennich <daniel.moennich@gmail.com>
2015-08-21 12:54:07 -0400

    Remover markers + refactoring.

--- public/javascript/buttonhandler.js
+++ public/javascript/buttonhandler.js
@@ -5,90 +5,87 @@ var days = [];
 var currentDay = {
 	hotel: "",
 	restaurant: [],
-	activities: []
+	activity: [],
+	markers: {}
 };
 
-// draw some locations on the map
- var hotelLocation = [];
-// var restaurantLocations = [];
-// var activityLocations = [];
+// entry types: hotel, restaurant, activity
+var createEntry = function (entryType) {
 
-$("#add_hotel").click(function() {
+	var entryName = $("#" +entryType+ " :selected").text(),
+		entryObject,
+		all_entries;
 
-	// getting hotel name from select
-	var hotel = $("#hotels :selected").text();
-	// inserting hotel into html
-	var itineraryItem = '<div class="itinerary-item"><span class="title">' + hotel + '</span>' +
-        '<button onclick=removeItem(this) data-type="hotel" ' + 'data-item="' + hotel +
-        '" class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
 
-    // replace existing hotel
-	if (currentDay.hotel) {
-		$("#currentHotel div:first").replaceWith(itineraryItem);
-	} else {
-		// add new entry in the hotel list
-		$("#currentHotel").append(itineraryItem);
+	switch (entryType) {
+		case "hotel":
+			all_entries = all_hotels;
+			break;
+		case "restaurant":
+			all_entries = all_restaurants;
+			break;
+		case "activity":
+			all_entries = all_activities;
+			break;
+		default:
+			throw Error("Type" + entryType + " does not exist.");
 	}
-	currentDay.hotel = hotel;
 
-	// find resturant location
-	for (var i = 0; i < all_hotels.length; i++){
-		if (all_hotels[i].name === hotel){
-			var location = all_hotels[i].place[0].location;
-			hotelLocation[0] = location;
-			drawLocation(location);
+
+	// check if entry is already in the list
+	if (entryType !== "hotel" && checkIfExists(currentDay[entryType], entryName)) {
+		return;
+	} else if (entryType === "hotel" && currentDay.hotel === entryName) {
+		return;
+	}
+
+
+	for (var i = 0; i < all_entries.length; i++){
+		if (all_entries[i].name === entryName){
+			entryObject = all_entries[i];
 			break;
 		}
 	}
+
+	// create the marker
+	var location = entryObject.place[0].location;
+	var markerIdentifier = entryType !== "hotel" ? entryType + "_" + entryName : entryType;
+
+
+	// inserting entry into html
+	var itineraryItem = '<div class="itinerary-item"><span class="title">' + entryName + '</span>' +
+        '<button onclick=removeItem(this) data-marker="' + markerIdentifier + '" data-type="hotel" ' + 'data-item="' + entryName +
+        '" class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
+
+
+    if (entryType === "hotel") {
+		if (currentDay.hotel) {
+			removeMarker(markerIdentifier);
+			$("#current_hotel div:first").replaceWith(itineraryItem);
+		} else {
+			$("#current_hotel").append(itineraryItem);
+		}
+		currentDay.hotel = entryName;
+    } else {
+		currentDay[entryType].push(entryName);
+		$("#current_" + entryType).append(itineraryItem);
+    }
+
+	drawLocation(location, markerIdentifier);
+};
+
+
+
+$("#add_hotel").click(function() {
+	createEntry("hotel");
 });
 
 $("#add_restaurants").click(function() {
-	var restaurant = $("#restaurant :selected").text();
-	// check if restaurant is already in the list
-	if (checkIfExists(currentDay.restaurant, restaurant)) {
-		return;
-	}
-	// add restaurant to the list
-	currentDay.restaurant.push(restaurant);
-	var itineraryItem = '<div class="itinerary-item"><span class="title">' + restaurant + '</span>' +
-        '<button onclick=removeItem(this) data-type="restaurant" data-item= "' + restaurant + '" class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
-
-	// add new entry in the restaurant list
-	$("#restaurantList").append(itineraryItem);
-	
-	// find resturant location
-	for (var i = 0; i < all_restaurants.length; i++){
-		if (all_restaurants[i].name === restaurant){
-			var location = all_restaurants[i].place[0].location;
-			drawLocation(location);
-		}
-	}
+	createEntry("restaurant");
 });
 
 $("#add_activities").click(function() {
-	var activities = $("#activity :selected").text();
-	
-	// check if activity is already in the list
-	if (checkIfExists(currentDay.activities, activities)) {
-		return;
-	}
-	// add new activity to the list
-	currentDay.activities.push(activities);
-
-	var itineraryItem = '<div class="itinerary-item"><span class="title">' + activities + '</span>' +
-    '<button onclick=removeItem(this) data-type="activities" data-item="' + activities + '" class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
-
-	// add new entry in the activity list
-	$("#activityList").append(itineraryItem);
-
-
-	// find activity location
-	for (var i = 0; i < all_activities.length; i++){
-		if (all_activities[i].name === activities){
-			var location = all_activities[i].place[0].location;
-			drawLocation(location);
-		}
-	}
+	createEntry("activity");
 });
 
 var checkIfExists = function (arr, value) {
@@ -97,20 +94,13 @@ var checkIfExists = function (arr, value) {
 	});
 };
 
-function drawLocation (location, opts) {
-	if (typeof opts !== 'object') {
-		opts = {};
-	}
+function drawLocation (location, markerIdentifier) {
+	var opts = {};
 	opts.position = new google.maps.LatLng(location[0], location[1]);
 	opts.map = map;
 	var marker = new google.maps.Marker(opts);
+	currentDay.markers[markerIdentifier] = marker;
 }
-
-
-
-// $("#currentHotel").on("click", function() {
-// 	console.log("button clicked");
-// });
 
 
 function removeItem(button){
@@ -118,17 +108,25 @@ function removeItem(button){
 	var name = $(button).attr("data-item");
 	var type = $(button).attr("data-type");
 
-	if (type === currentDay.hotel) { 
-		currentDay.hotel = "";
-	} 
+	if (type === "hotel") {
+		currentDay.hotel = undefined;
+	} else {
+		var index = currentDay[type].indexOf(name);
+		var removedItem = currentDay[type].splice(index, 1);
+	}
 
-
-	var index = currentDay[type].indexOf(name);
-	var removedItem = currentDay[type].splice(index, 1)
+	var markerIdentifier = $(button).attr("data-marker");
+	removeMarker(markerIdentifier);
+	// currentDay.markers[markerIdentifier].setMap(null);
+	// delete currentDay.markers[markerIdentifier];
 
 }
 
 
+var removeMarker = function (markerIdentifier) {
+	currentDay.markers[markerIdentifier].setMap(null);
+	delete currentDay.markers[markerIdentifier];
+};
 
 
 
